bb318b5f75e3f18a5868a4161482b2d3b6826927
D R <metricstream@gmail.com>
2026-05-11 10:02:50 -0700

    changed default theme

--- src/rust/show.rs
+++ src/rust/show.rs
@@ -24,7 +24,7 @@ impl WebPage for Src {
             "prohibited".to_string()
         };
         page_map.insert("path", src);
-        page_map.insert("theme", web.param("theme").unwrap_or("monokai".to_string()));
+        page_map.insert("theme", web.param("theme").unwrap_or("merbivore".to_string()));
         page_map.insert("line", web.param("line").unwrap_or_default());
         page_map.insert("col", web.param("pos").unwrap_or_default());
         page_map.insert("content", html_encode(&content).to_string());
